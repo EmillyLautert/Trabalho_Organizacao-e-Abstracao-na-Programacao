350d0e3607305f9dd2572a5723f30e062c6af71e
Camila Camposs <camposcamila27640@gmail.com>
2026-03-15 10:28:23 -0300

    Implementação da classe Produto para controle de estoque

--- classesMinimas/produto.py
+++ classesMinimas/produto.py
@@ -1,4 +1,5 @@
 class Produto:
+
     def __init__(self, idProduto, nomeProduto, quantidadeEstoque, precoProduto):
         self.idProduto = idProduto
         self.nomeProduto = nomeProduto
@@ -6,4 +7,7 @@ class Produto:
         self.precoProduto = precoProduto
 
     def __str__(self):
-        return f"ID: {self.idProduto}, Nome: {self.nomeProduto}, Quantidade em Estoque: {self.quantidadeEstoque}, Preço: R${self.precoProduto}"
+        return f"ID: {self.idProduto}, Nome: {self.nomeProduto}, Quantidade: {self.quantidadeEstoque}, Preço: R${self.precoProduto:.2f}"
+
+    def to_csv(self):
+        return f"{self.idProduto},{self.nomeProduto},{self.quantidadeEstoque},{self.precoProduto}"
