cfe792ebcae15c8d1b423be6f8fff9c17aca2355
Camila Camposs <camposcamila27640@gmail.com>
2026-03-15 10:12:58 -0300

    Implementação da classe Cliente conforme requisitos do sistema

--- classesMinimas/cliente.py
+++ classesMinimas/cliente.py
@@ -1,7 +1,11 @@
 class Cliente:
+
     def __init__(self, idCliente, nomeCliente):
         self.idCliente = idCliente
         self.nomeCliente = nomeCliente
-        
+
     def __str__(self):
         return f"ID: {self.idCliente}, Nome: {self.nomeCliente}"
+
+    def to_csv(self):
+        return f"{self.idCliente},{self.nomeCliente}"
