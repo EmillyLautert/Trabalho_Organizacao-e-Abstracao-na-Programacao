2f5eb3b8dbfe4335939d55a0dc355abc1428d9dc
Camila Camposs <camposcamila27640@gmail.com>
2026-03-15 10:39:34 -0300

    Implementação da classe Venda para registro das transações

--- classesMinimas/venda.py
+++ classesMinimas/venda.py
@@ -1,10 +1,14 @@
 class Venda:
-    def __init__(self, idVenda, cliente, produto, quantidade, valorTotal):
+
+    def __init__(self, idVenda, cliente, produto, quantidade):
         self.idVenda = idVenda
         self.cliente = cliente
         self.produto = produto
         self.quantidade = quantidade
-        self.valorTotal = valorTotal
-        
+        self.valorTotal = quantidade * produto.precoProduto
+
     def __str__(self):
-        return f"ID: {self.idVenda}, Cliente: {self.cliente.nomeCliente}, Produto: {self.produto.nomeProduto}, Quantidade: {self.quantidade}, Valor Total: R${self.valorTotal}"
+        return f"ID: {self.idVenda}, Cliente: {self.cliente.nomeCliente}, Produto: {self.produto.nomeProduto}, Quantidade: {self.quantidade}, Valor Total: R${self.valorTotal:.2f}"
+
+    def to_csv(self):
+        return f"{self.idVenda},{self.cliente.idCliente},{self.produto.idProduto},{self.quantidade},{self.valorTotal}"
